710551a711af4cdad0a794c0eb053ef983b10b2e
shadowgarden1314 <131418013+shadowgarden1314@users.noreply.github.com>
2024-03-29 20:45:55 +0800

    Add files via upload

---
+++ works.js
@@ -0,0 +1,104 @@
+
+// 定义存放图片链接的数组
+const imageGroups = {
+  'background_mobile': [
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/4c49b08bd358d9f0471e38474aaa7657.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/8327d3a7fd4a92f0a872c3c7fb2fed2e.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/9162afa9d023072a694e98aa8cd55151.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/QQ%E5%9B%BE%E7%89%8720240207031915.jpg',
+
+  ],
+  'background_tablet': [
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/f9ccc8e0c95b4d88afe570e2690f379c.jpg',
+  ],
+  'background_pc': [
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/QQ%E5%9B%BE%E7%89%8720240218004523.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E7%9C%9F%E7%90%86-Mari_%E8%8A%B1%E7%81%AB_116905096_p0.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E8%8E%8E%E9%B1%BCLynn_%E5%85%A5%E6%A2%A6_117207832_p1.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E8%8E%8E%E9%B1%BCLynn_%E5%85%A5%E6%A2%A6_117207832_p0.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E8%BE%B0%E6%9A%AEsora_%E8%87%B4%E4%BB%A5%E6%97%A0%E7%91%95%E4%B9%8B%E4%BA%BA_100732285_p0.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E7%88%B1%E7%94%BB%E7%94%BB%E7%9A%84%E6%B7%B1%E5%A4%8F_%E4%BD%A0%E8%BF%99%E5%AE%B6%E4%BC%99%EF%BC%8C%E5%9C%A8%E5%81%B7%E7%9C%8B%E6%88%91%E5%AF%B9%E5%90%A7%EF%BC%81_116602575_p0.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E7%88%B1%E5%90%83%E5%A5%B6%E7%9B%96_%E7%9F%A5%E6%9B%B4%E9%B8%9F_117075670_p0.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E7%88%B1%E5%90%83%E5%A5%B6%E7%9B%96_%E6%A2%A6%E6%B8%B8%E9%B1%BC_117298295_p0.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E3%81%82%E3%82%8B%E3%81%A6%E3%82%89_%F0%9F%92%99_111334049_p0.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/%E3%81%82%E3%82%8B%E3%81%A6%E3%82%89_zeta%E3%81%A1%E3%82%83%E3%82%93_102875400_p0.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/Yolanda_%E7%92%83%E6%9C%88_104827575_p0.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/xUDe_Acheron_117062300_p0.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/Vardan_%E5%A7%8B%E6%BA%90%EF%BC%8F%E7%B5%82%E7%84%89_105463056_p2.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/Vardan_%E5%A7%8B%E6%BA%90%EF%BC%8F%E7%B5%82%E7%84%89_105463056_p1.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/Vardan_%E5%A7%8B%E6%BA%90%EF%BC%8F%E7%B5%82%E7%84%89_105463056_p0.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/Vardan_%E8%8A%B1%E7%81%AB_115727998_p0.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/F197C6511D515C9E2D03720C9F15602B.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/F26ACF2261C41F1DDF2A46C70CDCE804.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/C6DC4F6D6B4BBB966FA4850EBC57AF03.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/ATDAN-_Spring_116570030_p0.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/93558017E103F9684DF6A2489B9457E4.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/6090444C5595B85DA58DA23240A6C89D.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/691868E425238F35E115FE2EBDA790EF.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/54484E26E4278BF7A1E6DF1AC85FB80D.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/1822C2596F9F3655825220F58692BB3A.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/94B7DD91CA4189D8D085D99815BF3CE4.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/4D3445CA4E0C31C8E02267255DC19869.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/3BD8B1C092218F33E1EDAD80E99E9206.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/1C13C47F83D1F7AC0DF56FC06BCA2BB4.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/illust_104129223_20240308_004059.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/illust_104128656_20240308_004112.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/5.jpg',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/4.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/2.png',
+'https://cdn.jsdelivr.net/gh/shadowgarden1314/imagegeren/1.jpg'
+  ]
+};
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  // 获取传入的 id 参数
+  const url = new URL(request.url);
+  const id = url.searchParams.get('id');
+
+  // 如果存在 id 参数，选择对应图片组
+  let selectedGroup;
+  if (id && imageGroups[id]) {
+    selectedGroup = imageGroups[id];
+  } else {
+    // 如果没有指定 id，则随机选择所有图片组中的一张图片
+    const allImages = Object.values(imageGroups).flat();
+    selectedGroup = allImages;
+  }
+
+  // 随机选择图片链接
+  const randomImage = selectedGroup[Math.floor(Math.random() * selectedGroup.length)];
+
+  // 获取图片内容
+  const imageResponse = await fetch(randomImage);
+  const imageContent = await imageResponse.arrayBuffer();
+
+  // 设置 Content-Type
+  const imageType = getImageType(randomImage);
+  const contentType = imageType ? `image/${imageType}` : 'application/octet-stream';
+
+  // 构建响应
+  const headers = {
+    'Content-Type': contentType,
+    'Content-Length': imageContent.byteLength,
+    'Accept-Ranges': 'bytes'
+  };
+
+  return new Response(imageContent, { headers });
+}
+
+function getImageType(url) {
+  const extension = url.split('.').pop().toLowerCase();
+  if (extension === 'jpg' || extension === 'jpeg') {
+    return 'jpeg';
+  } else if (extension === 'png') {
+    return 'png';
+  } else if (extension === 'gif') {
+    return 'gif';
+  } else {
+    return null;
+  }
+}
